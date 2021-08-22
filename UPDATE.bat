83f722789ab178b596d342d2bf6c63f50b1984a5
JaffLegend <89238099+JaffLegend@users.noreply.github.com>
2021-08-22 12:49:23 +1000

    Double pause error fix

--- UPDATE.bat
+++ UPDATE.bat
@@ -1,39 +1,5 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFBBVVgmLAE+1EbsQ5+n//Na3u1kVW+wPfYvS2/nfHNAK/k3re6Yo0nNb1ZowOSt8XyaODg==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
-@echo off
+
 echo 'Updating PythonPedia'
-pause
 del data.txt
 pause
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/JaffLegend/PythonPedia/v1/data.txt -Outfile data.txt"
