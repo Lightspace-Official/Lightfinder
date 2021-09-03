5452dcfd29b4d93f2f981641d63ad25774ff43f0
JaffLegend <89238099+JaffLegend@users.noreply.github.com>
2021-09-03 12:17:15 +1000

    Update UPDATE.bat

--- UPDATE.bat
+++ UPDATE.bat
@@ -1,9 +1,8 @@
 @echo off
-echo 'Updating Lightfinder'
+echo Updating Lightfinder
 del data.txt
 del Lightfinder.py
-pause
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/JaffLegend/PythonPedia/v1/data.txt -Outfile data.txt"
 powershell -Command "Invoke-Webrequest https://raw.githubusercontent.com/JaffLegend/Lightfinder/v1/Lightfinder.py -Outfile Lightfinder.py"
-echo "Done!"
+echo Done!
 pause
