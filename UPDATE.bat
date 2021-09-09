909b7a1efa4007787225c1db28cc4a7f5ac4a7db
JaffLegend <89238099+JaffLegend@users.noreply.github.com>
2021-09-09 11:45:36 +1000

    Add files via upload

--- UPDATE.bat
+++ UPDATE.bat
@@ -2,7 +2,7 @@
 echo Updating Lightfinder
 del data.txt
 del Lightfinder.py
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/JaffLegend/PythonPedia/v1/data.txt -Outfile data.txt"
-powershell -Command "Invoke-Webrequest https://raw.githubusercontent.com/JaffLegend/Lightfinder/v1/Lightfinder.py -Outfile Lightfinder.py"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/LightOrg-Official/Lightfinder/v1/data.txt -Outfile data.txt"
+powershell -Command "Invoke-Webrequest https://raw.githubusercontent.com/LightOrg-Official/Lightfinder/v1/Lightfinder.py -Outfile Lightfinder.py"
 echo Done!
 pause
