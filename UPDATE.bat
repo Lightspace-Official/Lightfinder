32e475a8508b5b3889408e7a5d5e124c58f9b079
JaffLegend <89238099+JaffLegend@users.noreply.github.com>
2021-09-09 11:56:47 +1000

    Add files via upload

--- UPDATE.bat
+++ UPDATE.bat
@@ -1,8 +1,11 @@
 @echo off
-echo Updating Lightfinder
+echo Updating Lightfinder...
 del data.txt
 del Lightfinder.py
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/LightOrg-Official/Lightfinder/v1/data.txt -Outfile data.txt"
 powershell -Command "Invoke-Webrequest https://raw.githubusercontent.com/LightOrg-Official/Lightfinder/v1/Lightfinder.py -Outfile Lightfinder.py"
 echo Done!
 pause
+echo Do you want to launch Lightfinder? If no, you are free to exit.
+pause
+START Lightfinder.exe
